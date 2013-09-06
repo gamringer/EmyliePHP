c907b9f8cf31fc5f353354d60646b68e5379f966
gamringer <g.amringer@gmail.com>
2013-09-06 09:36:13 -0400

    missing pushFile() for file download

--- Emylie/Core/Stack/Controller.php
+++ Emylie/Core/Stack/Controller.php
@@ -46,5 +46,22 @@ namespace Emylie\Core\Stack {
 			$this->display_result_container = $stack;
 		}
 
+		public function pushFile($path, $name){
+			if (file_exists($path)) {
+				header('Content-Description: File Transfer');
+				header('Content-Type: application/octet-stream');
+				header('Content-Disposition: attachment; filename="'.$name.'"');
+				header('Content-Transfer-Encoding: binary');
+				header('Expires: 0');
+				header('Cache-Control: must-revalidate');
+				header('Pragma: public');
+				header('Content-Length: ' . filesize($path));
+				ob_clean();
+				flush();
+				readfile($path);
+				exit;
+			}
+		}
+
 	}
 }
