2e3d1345735a5ea0de7d7ba104fd2f884972a1d2
gamringer <g.amringer+github@gmail.com>
2014-09-20 23:35:13 -0400

    provide getallheaders() shim

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,3 +4,7 @@ include dirname(__FILE__).'/../SplClassLoader.php';
 
 (new \SplClassLoader('Emylie\Test', dirname(__FILE__).'/../tests'))->register();
 (new \SplClassLoader('Emylie', dirname(__FILE__).'/../src'))->register();
+
+if (!function_exists('getallheaders')) {
+	function getallheaders(){return [];}
+}
